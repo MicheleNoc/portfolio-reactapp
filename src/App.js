247696eecb25b325cf16ba802711250f728d6668
Michele Nocchi <michelenocchi@MacBook-Pro.local>
2024-09-27 22:58:04 +0200

    aggiunta navbar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
-import Navigation from './components/Navigation'; 
+
+
 function App() {
   return (
     <Router basename="/portfolio-reactapp">
-      <Navigation />  {/* Aggiungi il componente Navigation */}
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
