052d4f0fe1bc9341940ed226187ba32eb5778067
Michele Nocchi <michelenocchi@MacBook-Pro.local>
2024-09-27 23:05:53 +0200

    aggiunta navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,9 @@ function Navbar() {
         <div>
 <nav class="bg-white border-gray-200 dark:bg-gray-900 fixed-top">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <li><Link class="flex items-center space-x-3 rtl:space-x-reverse font-semibold whitespace-nowrap dark:text-white" to="/">💻 Michele Nocchi</Link></li>
-        
+    <li><Link class="flex items-center space-x-3 rtl:space-x-reverse font-semibold whitespace-nowrap dark:text-white" to="/"> <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"> 💻 Michele Nocchi</span>
+    </Link></li>
+       
 
     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
       <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
